a0143739d2095451b29e818e2695a41e40ad3bee
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-11-30 15:46:24 +0700

    Add doc string

---
+++ colab_ssh/__init__.py
@@ -0,0 +1,1 @@
+from .main import setupSSH, loop_forever
--- colab_ssh/main.py
+++ colab_ssh/main.py
@@ -1,3 +1,5 @@
+import time
+
 from colab_ssh.config import config_root_password, install_common_tool
 from colab_ssh.ssh import config_ssh_server, parse_public_key
 from colab_ssh.tunel import config_argo_tunnel
@@ -5,6 +7,17 @@ from colab_ssh.utils import check_gpu_available
 
 
 def setupSSH(public_key):
+    """
+    Setup an SSH tunel to the current Colab notebook instance with ssh public key authentication
+
+    Arguments:
+        public_key:
+            - (str): The public key that will be able to authenticate the SSH connection
+            - (List[str]): A list of public keys that will be able to authenticate the SSH connection
+            - (str): Link to a text file (authorized_keys) that cotains all the public keys that will be able to authenticate the SSH connection
+    
+    After about 2 minutes of running, the bash command to initialize the SSH connection will be print out
+    """
     public_key = parse_public_key(public_key)
 
     if not check_gpu_available():
@@ -23,3 +36,11 @@ def setupSSH(public_key):
     # Config Argo Tunnel
     msg = config_argo_tunnel(msg)
     print(msg)
+
+
+def loop_forever():
+    """
+    Utility function to keep the colab notebook from disconnecting the kernel.
+    """
+    while True:
+        time.sleep(15)
