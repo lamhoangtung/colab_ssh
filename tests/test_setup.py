e03f8665afa66bd2906fd24671717294438195e1
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-12-01 12:50:20 +0700

    lol 11

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -11,11 +11,13 @@ class TestSetup(unittest.TestCase):
     def test_setup(self):
         ssh_public_key = os.environ.get("TEST_SSH_PUBLIC_KEY")
         setup_ssh(ssh_public_key)
+        assert 1 == 1
 
     def test_setup_with_notifications(self):
         webhook_address = os.environ.get("TEAMS_WEBHOOK_ADDRESS")
         ssh_public_key = os.environ.get("TEST_SSH_PUBLIC_KEY")
         setup_ssh(ssh_public_key, webhook_address)
+        assert 1 == 1
 
 if __name__ == '__main__':
     unittest.main()
