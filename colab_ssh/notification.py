c6eb26bd1d0f53c99e7062efe1174940cb0b6302
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-12-01 14:29:39 +0700

    Proper push notification test with secret env variable

--- colab_ssh/notification.py
+++ colab_ssh/notification.py
@@ -1,5 +1,6 @@
 """Send push notification to Microsoft Teams using webhook. Just to manage all the instance"""
 
+import os
 from typing import Dict
 
 import pymsteams
@@ -11,8 +12,12 @@ def send_notification_to_microsoft_teams(webhook_address: str, spec: Dict):
     """
 
     message = pymsteams.connectorcard(webhook_address)
-    message.text(
-        "Hi @channel, a new colab spot instance was created! Here was it configuration:")
+    if os.environ.get("IS_TESTING_CI") is not None:
+        message.text(
+            "Hi @channel, a new CI/CD test for the colab ssh pip package was initialized! Here was it configuration:")
+    else:
+        message.text(
+            "Hi @channel, a new colab spot instance was created! Here was it configuration:")
 
     section = pymsteams.cardsection()
     section.addFact("CPU", spec['cpu'])
